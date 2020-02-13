4635684175def5d22681452678443c0c566e077b
Alex Indries <alexandru.indries@fortech.ro>
2020-02-13 09:34:59 +0200

    First Commit

---
+++ src/test/java/com/example/CompanyTogether/CompanyTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompanyTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
